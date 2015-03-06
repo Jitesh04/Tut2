218722ed461b73036d0cd40d3d240273001bc7b7
Jitesh04 <213503161@stu.ukzn.ac.za>
2015-03-06 21:58:27 +0200

    Create main

---
+++ Tut2/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	double f1Numerator;
+	double f1Denominator;
+	double f2Numerator;
+	double f2Denominator;
+
+	cout << "Enter Numerator of fraction 1: ";
+	cin >> f1Numerator;
+
+	cout << "Enter Denominator of fraction 1: ";
+	cin >> f1Denominator;
+
+	cout << "Enter Numerator of fraction 2: ";
+	cin >> f1Numerator;
+
+	cout << "Enter Denominator of fraction 2: ";
+	cin >> f1Denominator;
+
+	return 0;
+}
